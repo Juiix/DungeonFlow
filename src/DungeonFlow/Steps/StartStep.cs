b09607376d3c1be5182d3780dea438eeb0e51b9a
Tucker Ulrich <5263190+Juiix@users.noreply.github.com>
2026-04-16 12:04:14 -0600

    Start step fix

--- src/DungeonFlow/Steps/StartStep.cs
+++ src/DungeonFlow/Steps/StartStep.cs
@@ -9,7 +9,7 @@ public sealed class StartStep(int[] roomIds) : IGenerationStep
 		var tries = 0;
 		while (tries++ < 100)
 		{
-			var roomId = generator.Random.Next(0, _roomIds.Length);
+			var roomId = _roomIds[generator.Random.Next(0, _roomIds.Length)];
 			var center = generator.Graph.Size / 2;
 			if (generator.TryPlace(out _, roomId, center))
 				break;
